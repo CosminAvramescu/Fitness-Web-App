6bf5d83ce055f08ffbb12c8fc048d8676eb408fb
TStefan44 <george.tudose2504@stud.acs.upb.ro>
2023-04-21 01:22:03 +0300

    Bootstrap setup

---
+++ frontend/src/Component/Homepage/Homepage.js
@@ -0,0 +1,26 @@
+function Homepage() {
+    return (
+        <div>
+            <header>
+                <h1>Fitness Web Page</h1>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Workouts</a></li>
+                    <li><a href="#">Nutrition</a></li>
+                </ul>
+            </nav>
+            <main>
+                <h2>Welcome to our Fitness Web Page!</h2>
+                <p>We offer a variety of workouts and nutrition plans to help you achieve your fitness goals.</p>
+                <img src="https://via.placeholder.com/300x200" alt="Fitness Image" />
+            </main>
+            <footer>
+                <p>&copy; 2023 Fitness Web Page. All rights reserved.</p>
+            </footer>
+        </div>
+    );
+}
+export default Homepage
