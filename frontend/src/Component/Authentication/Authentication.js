98124f349dd1238f4ea1913b8a158f7df58c45d5
TStefan44 <george.tudose2504@stud.acs.upb.ro>
2023-04-25 15:24:24 +0300

    Minor refactor to Authentication classes

--- frontend/src/Component/Authentication/Authentication.js
+++ frontend/src/Component/Authentication/Authentication.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import Col from "react-bootstrap/Col";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 class Authentication extends Component {
     state = {
@@ -9,8 +12,30 @@ class Authentication extends Component {
     render() {
         return (
             <div>
-                {this.state.value === 1? <SignIn/> : <SignUp/>}
+                <Container fluid="md" className="p-5">
+                    {this.state.value === 1?
+                        this.renderSignIn() : this.renderSignUp()
+                    }
+                </Container>
             </div>
+        );
+    }
+
+    renderSignIn() {
+        return (
+            <Row className="justify-content-md-center">
+                <Col><SignIn/></Col>
+                <Col>"Image Here"</Col>
+            </Row>
+        );
+    }
+
+    renderSignUp() {
+        return (
+            <Row className="justify-content-md-center">
+                <Col><SignUp/></Col>
+                <Col>"Image Here"</Col>
+            </Row>
         );
     }
 }
--- frontend/src/Component/Authentication/SignIn.js
+++ frontend/src/Component/Authentication/SignIn.js
@@ -1,39 +1,26 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 class SignIn extends Component {
     render() {
         return (
-            <div>
-                <Container fluid="md" className="p-5">
-                    <Row className="justify-content-md-center">
-                        <Col>
-                            <Form>
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Control type="email" placeholder="Email or Profile" />
-                                </Form.Group>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Email or Profile" />
+                </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Control type="password" placeholder="Password" />
-                                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                    <Form.Check type="checkbox" label="Remember me" />
-                                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Remember me" />
+                </Form.Group>
 
-                                <Button variant="primary" type="submit">
-                                    SIGN IN
-                                </Button>
-                            </Form>
-                        </Col>
-                        <Col>"Image Here"</Col>
-                    </Row>
-
-                </Container>
-            </div>
+                <Button variant="primary" type="submit">
+                    SIGN IN
+                </Button>
+            </Form>
         );
     }
 }
