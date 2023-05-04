d321f026ab02dbaa0898e38c220a55ecc87cbc82
CosminAvramescu <cosmin.avramescu11@gmail.com>
2023-05-04 23:04:19 +0300

    mine

--- backend/src/main/java/com/example/backend/register/RegisterController.java
+++ backend/src/main/java/com/example/backend/register/RegisterController.java
@@ -4,19 +4,17 @@ import com.example.backend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1")
 @Component
+@CrossOrigin(origins="*")
 public class RegisterController {
     @Autowired
     private RegisterService registerService;
 
-    @PostMapping("/register")
+    @PostMapping("register")
     public ResponseEntity<String> register(@RequestBody User user){
         ResponseEntity responseEntity = registerService.register(user);
         return new ResponseEntity(responseEntity, responseEntity.getStatusCode());
--- frontend/src/Component/Authentication/SignUp.js
+++ frontend/src/Component/Authentication/SignUp.js
@@ -74,7 +74,7 @@ class SignUp extends Component {
             this.state.birthday
         )
 
-        let url = "http://localhost:8082/user/addUser"
+        let url = "http://localhost:8082/api/v1/register"
         let response = await axios.post(url, user,
             {
                 headers: {
